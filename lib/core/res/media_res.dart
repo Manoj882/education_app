e5ab050e5117c6ec1e302a8e02550c3fd1325035
Manoj882 <manojbk488@gmail.com>
2023-09-02 08:43:31 +0545

    feat/create page content entity

--- lib/core/res/media_res.dart
+++ lib/core/res/media_res.dart
@@ -3,10 +3,19 @@ class MediaRes {
   const MediaRes._();
 
   static const _baseIcons = 'assets/icons';
+  static const _baseVectors = 'assets/vectors';
 
   static const atom = '$_baseIcons`/atom.png';
 
   static const onBoardingBackground = '';
 
   static const pageUnderConstruction = '';
+
+
+  //vectors
+  static const casualReading = '$_baseVectors/casual_reading.png';
+  static const casualLife = '$_baseVectors/casual_life.png';
+  static const casualMeditationScience = '$_baseVectors/casual_meditation_science.png';
+
+
 }
